1d126852e42c03427f36339d26f8adaeb3b8e346
Akash Chaudhari <138999370+akashch1512@users.noreply.github.com>
2024-12-18 09:38:51 +0000

    final

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/workspaces/client/src/app/layout\":[\"static/media/f28c0d0ddfdf7c25-s.p.woff2\",\"static/media/f884d4ea94220255-s.p.woff2\",\"static/media/e76854c588de135c-s.p.woff2\",\"static/media/e94cfb95acf534f2-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
